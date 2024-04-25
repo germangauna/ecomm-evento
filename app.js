ad37714c885a6f0de0c72f7e85ac482543b42d1d
GermanG <germangauna88@gmail.com>
2024-04-24 22:57:29 -0300

    creacion de las cards

---
+++ app.js
@@ -0,0 +1,68 @@
+let shopContent = document.getElementById("shopContent");
+
+// array
+
+const productos = [
+    {
+        id: 1,
+        nombre: "Harina",
+        precio: 50, 
+        img: "https://us.123rf.com/450wm/angorius/angorius1311/angorius131100017/23821222-peque%C3%B1a-bolsa-aislada-con-harina-blanca-sobre-el-fondo-blanco.jpg?ver=6",
+    },
+    {
+        id: 2,
+        nombre: "Galletitas",
+        precio: 100, 
+        img: "https://salpimenta.com.ar/wp-content/uploads/2017/01/galletitas.jpg",
+    },
+    {
+        id: 3,
+        nombre: "Cerveza",
+        precio: 150, 
+        img: "https://i.blogs.es/281c38/cerveza2/1366_2000.jpg",
+    },
+    {
+        id: 4,
+        nombre: "Leche",
+        precio: 200, 
+        img: "https://static7.depositphotos.com/1001069/716/i/950/depositphotos_7169901-stock-photo-milk-bottle.jpg",
+    },
+];
+
+let carrito = [];
+
+//creamos funcion para que aprescan los productos en el DOM
+
+productos.forEach((product) =>{
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+                        <img src="${product.img}">
+                        <h3> ${product.nombre}</h3>
+                        <p> ${product.precio}</p>
+    `;
+    shopContent.append(content);
+
+//creamos el boton para los elementos
+
+    let comprar = document.createElement("button");
+    comprar.className = "comprar";
+    comprar.innerText = "comprar";
+
+    content.append(comprar);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
