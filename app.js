47d6349d2a8d9d5859c6eb2f50a5996c66aecdb6
GermanG <germangauna88@gmail.com>
2024-04-25 22:56:33 -0300

    modal

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let shopContent = document.getElementById("shopContent");
-
-
+let shopContent = document.getElementById("shopContent"); // div contenido de las cards
+let verCarrito = document.getElementById("verCarrito");// div del carrito
+let modalContainer = document.getElementById("modal-container")// div del modal
 
 let carrito = [];
 
@@ -24,8 +24,45 @@ productos.forEach((product) =>{
 
     content.append(comprar);
 
+    //le pasamos un evento ("click ") en una funcion a traves del metodo push
+     comprar.addEventListener("click", () => {
+        carrito.push({
+            id: product.id,
+            nombre: product.nombre,
+            img: product.img,
+            precio:product.precio,
+        });
+
+       // console.log(carrito);
+
+     });
+
 });
 
+// pasamos el evento a verCarrito
+
+verCarrito.addEventListener("click", () =>{
+    //console.log("holaaa funciona");
+    // hacemos esete consolog y clickeamos el emoji del carrito que tenemos en el navbar
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+                            <h1 class="modal-header-title">Carrito</h1>
+    ` 
+    //le pasamos el append
+    modalContainer.append(modalHeader);
+
+    //boton del modal header x
+
+    const modalButton = document.createElement("h1");
+    modalButton.innerText = "x";
+    modalButton.className = "modal-header-button";
+
+//modalHeader es el padre del modalButton
+    modalHeader.append(modalButton);
+    
+
+});
 
 
 
