30199ad35fd3131b8c5066c876f6cdc0fcc36a3c
GermanG <germangauna88@gmail.com>
2024-04-29 20:29:48 -0300

    arreglos

--- app.js
+++ app.js
@@ -43,6 +43,12 @@ productos.forEach((product) =>{
 
 verCarrito.addEventListener("click", () =>{
     //console.log("holaaa funciona");
+
+    //inner con un sytrig vacio para que no repita el carrito cuano lo llamamos
+    modalContainer.innerHTML = "";
+
+    modalContainer.style.display = "flex";//para que vuelva aprecer el carrito
+
     // hacemos esete consolog y clickeamos el emoji del carrito que tenemos en el navbar
     const modalHeader = document.createElement("div");
     modalHeader.className = "modal-header";
@@ -58,13 +64,46 @@ verCarrito.addEventListener("click", () =>{
     modalButton.innerText = "x";
     modalButton.className = "modal-header-button";
 
+    //pasamos un evento para que cuando apretemose l boton desaparesca
+    modalButton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+
 //modalHeader es el padre del modalButton
     modalHeader.append(modalButton);
-    
 
+//body del modal, lo recorremos con un forEach
+
+carrito.forEach((product) => {
+    let carritoContent = document.createElement("div");
+    carritoContent.className = "modal-content";
+    carritoContent.innerHTML = `
+                                <img src="${product.img}">
+                                <h3>${product.nombre}</h3>
+                                <p>${product.precio}</p>
+                                `;
+
+  modalContainer.append(carritoContent);                                 
 });
 
+//creamos la funcion donde va a sumar al total
 
+const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+//creamos el footer
+
+const totalBuying = document.createElement("div");
+totalBuying.className = "total-container";
+totalBuying.innerHTML = `total a pagar ${total}`
+
+modalContainer.append(totalBuying);
+
+
+
+
+
+
+});
 
 
 
